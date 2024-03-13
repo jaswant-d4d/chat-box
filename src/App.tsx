42a8657dbc93b393f48ee99e9c8118f8b5cca205
jaswant-d4d <jaswant@1234#>
2024-03-13 15:54:27 +0530

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import Login from './screens/auth/Login';
 import Signup from './screens/auth/Signup';
 import SingleChat from './screens/chat-box/SingleChat';
 import ProtectedRoute from './routes/PrivateRoute';
+import Layout from './layouts';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/chat" element={<ProtectedRoute><SingleChat /></ProtectedRoute>} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="/chat" element={<ProtectedRoute><SingleChat /></ProtectedRoute>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Route>
       </Routes>
     </div>
   );
